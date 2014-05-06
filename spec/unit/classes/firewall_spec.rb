3cef9c75446a7eadc5d123550c1830faeb323b62
Niklas Hofer <niklas+dev@lanpartei.de>
2014-05-06 22:38:43 +0200

    spec: s/include_class/contain_class/ for deprecation

--- spec/unit/classes/firewall_spec.rb
+++ spec/unit/classes/firewall_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe 'firewall', :type => :class do
   context 'kernel => Linux' do
     let(:facts) {{ :kernel => 'Linux' }}
-    it { should include_class('firewall::linux') }
+    it { should contain_class('firewall::linux') }
   end
 end
